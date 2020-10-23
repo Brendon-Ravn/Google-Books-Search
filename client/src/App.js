1fa89037c7afb2d11be34939505f173b2d6d2b67
Brendon Conatser <b.conatser21@gmail.com>
2020-10-23 01:32:36 -0500

    heroku

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Search from './components/pages/Search';
 import Saved from './components/pages/Saved';
-import "./app.css";
 
 function App() {
   return (
